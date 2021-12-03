2c5a14a6731a002bb34abb8d7d1954845bc131c4
eddiejan <edian@elseif.it>
2021-12-03 17:20:31 +0100

    initial commit

--- DependencyInjection/NetsuiteRepositoryPass.php
+++ DependencyInjection/NetsuiteRepositoryPass.php
@@ -6,13 +6,14 @@ use Eddiejan\NetsuiteClient\NetsuiteClient;
 use Eddiejan\NetsuiteClient\Repository\NetsuiteAbstractRepository;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
 class NetsuiteRepositoryPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
         $container->register(NetsuiteAbstractRepository::class, NetsuiteAbstractRepository::class)
-            ->addArgument('%netsuite.client%')
+            ->addArgument(new Reference('netsuite.client'))
         ;
     }
 }
