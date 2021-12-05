d31c935a06d5850fe3a89fa2a6a2f2fb2582f181
eddiejan <edian@elseif.it>
2021-12-05 09:20:05 +0100

    initial commit

--- DependencyInjection/NetsuiteExtension.php
+++ DependencyInjection/NetsuiteExtension.php
@@ -18,10 +18,10 @@ class NetsuiteExtension extends Extension implements PrependExtensionInterface
     {
         $configs = $container->getExtensionConfig('framework');
 
-        $config['transports']['netsuite']['dsn'] = '%env(MESSENGER_TRANSPORT_DSN)%';
-        $config['routing'] = [NetsuiteMessage::class => 'netsuite'];
+        $config[3]['transports']['netsuite']['dsn'] = '%env(MESSENGER_TRANSPORT_DSN)%';
+        $config[3]['routing'] = [NetsuiteMessage::class => 'netsuite'];
 
-        $container->prependExtensionConfig('messenger', $config);
+        $container->prependExtensionConfig('framework', $config);
 
         dump($container->getExtensionConfig('framework'));
     }
