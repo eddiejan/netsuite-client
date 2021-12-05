32afabc1c48eaae318ce8b169b539ad3dd3485a2
eddiejan <edian@elseif.it>
2021-12-05 09:35:20 +0100

    initial commit

--- DependencyInjection/NetsuiteExtension.php
+++ DependencyInjection/NetsuiteExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class NetsuiteExtension extends Exphptension implements PrependExtensionInterface
+class NetsuiteExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container)
     {
