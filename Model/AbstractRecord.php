c9020e327d96772ee68e84025943098d8f235d17
eddiejan <edian@elseif.it>
2021-12-23 18:10:58 +0100

    WIP

--- Model/AbstractRecord.php
+++ Model/AbstractRecord.php
@@ -4,18 +4,26 @@ namespace Eddiejan\NetsuiteClient\Model;
 
 abstract class AbstractRecord
 {
-    public function __construct (
-        private ?string $id,
-        private ?string $externalId
-    ) {}
+    private ?string $id;
+    private ?string $externalId;
 
     public function getId(): ?string
     {
         return $this->id;
     }
 
+    public function setId(?string $id): void
+    {
+        $this->id = $id;
+    }
+
     public function getExternalId(): ?string
     {
         return $this->externalId;
+    }
+
+    public function setExternalId(?string $externalId): void
+    {
+        $this->externalId = $externalId;
     }
 }
